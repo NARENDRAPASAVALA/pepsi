05d140b044e011e08584e3de78218dc6f6be1aa9
NARENDRAPASAVALA <psplnarendra@gmail.com>
2020-12-23 11:03:44 +0530

    all screens

---
+++ screen6/screen6.js
@@ -0,0 +1,26 @@
+var startSeqs = {};
+var startNum = 0;
+
+function spin() {
+    console.log("inside the spin")
+}
+
+// jQuery FN
+$.fn.playSpin = function (options) {
+    if (this.length) {
+        if ($(this).is(':animated')) return; // Return false if this element is animating
+        startSeqs['mainSeq' + (++startNum)] = {};
+        $(this).attr('data-playslot', startNum);
+    }
+};
+
+$.fn.stopSpin = function () {
+    if (this.length) {
+        if (!$(this).is(':animated')) return; // Return false if this element is not animating
+        if ($(this)[0].hasAttribute('data-playslot')) {
+            $.each(startSeqs['mainSeq' + $(this).attr('data-playslot')], function(index, obj) {
+                obj['spinning'] = false;
+            });
+        }
+    }
+};
